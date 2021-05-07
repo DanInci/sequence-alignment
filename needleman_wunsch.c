42637f575685349f648ad82235a01a85517f04f0
Daniel Incicau <daniel_incicau@yahoo.com>
2021-05-08 02:04:51 +0300

    Read from file

--- needleman_wunsch.c
+++ needleman_wunsch.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MATCH 1
 #define MISMATCH -1
@@ -7,6 +8,12 @@
 
 #define MAX(a,b) (((a)>(b))?(a):(b))
 #define FIT_SCORE(a, b) (a == b ? MATCH : MISMATCH)
+
+struct Alignment {
+    int **score;
+    char *alignedA;
+    char *alignedB;
+};
 
 /*
  * MAX OF (FIT (MATCH OR MISMATCH), INSERT, DELETE) SCORES
@@ -26,20 +33,20 @@ int **needleman_wunsch_score(char *seqA, char *seqB, int lenA, int lenB) {
     int i, j, **score;
     char a, b;
     
-    // alocare memorie
+    // allocate memory for score matrix
     score = (int **) calloc((lenA + 1), sizeof(int *));
     for (i = 0; i <= lenA; i++) {
         score[i] = (int *) calloc((lenB + 1), sizeof(int));
     }
 
-    // initializat prima coloana
-    // all gaps => indexului coloanei * GAP score
+    // initlialize first column
+    // all gaps => column index * GAP score
     for (i = 0; i <= lenA; i++) {
         score[i][0] = i * GAP;
     }
 
-    // initializat prima linie
-    // all gaps => indexului liniei * GAP score
+    // initialize first row
+    // all gaps => row index * GAP score
     for (j = 0; j <= lenB; j++) {
         score[0][j] = j * GAP;
     }
@@ -47,7 +54,7 @@ int **needleman_wunsch_score(char *seqA, char *seqB, int lenA, int lenB) {
     int m = lenA + 1;
     int n = lenB + 1;
 
-    // traversare anti-diagonala, exceptand prima linie si prima coloana
+    // anti-diagonal traversal, except first row and column
     for (int slice = 2; slice < m + n - 1; slice++) {
         int z1 = slice <= n ? 1 : slice - n + 1;
         int z2 = slice <= m ? 1 : slice - m + 1;
@@ -61,49 +68,39 @@ int **needleman_wunsch_score(char *seqA, char *seqB, int lenA, int lenB) {
     return score;
 }
 
-void needleman_wunsch_align(char *seqA, char *seqB, int lenA, int lenB) {
-    int **score = needleman_wunsch_score(seqA, seqB, lenA, lenB);
+struct Alignment *needleman_wunsch_align(char *seqA, char *seqB, int lenA, int lenB) {
+    struct Alignment *alignment;
+    char *alignedA, *alignedB;
     int i, j, k;
-    char *alignedA, *alignedB, *alignedHelper;
 
-    // afiseaza matricea de scor 
-    for (i = 0; i <= lenA; i++) {
-        for (j = 0; j <= lenB; j++) {
-            printf("%3d ", score[i][j]);
-        }
-        printf("\n");
-    }
-
-    // alocare memorie
-    k = lenA + lenB; // alinierea poate sa fie de lungime maxima lenA + lenB
+    // allocate memory
+    alignment = (struct Alignment *) calloc(1, sizeof(struct Alignment *));
+    k = lenA + lenB; // maximum length of alignment is lenA + lenB
     alignedA = (char *) calloc((k + 1), sizeof(char *));
     alignedA[k] = '\0';
     alignedB = (char *) calloc((k + 1), sizeof(char *));
     alignedB[k] = '\0';
-    alignedHelper = (char *) calloc((k + 1), sizeof(char *));
-    alignedHelper[k] = '\0';
     k--;
+
+    alignment->score = needleman_wunsch_score(seqA, seqB, lenA, lenB);
 
     i=lenA;
     j=lenB;
 
     while (i > 0 && j > 0) {
-        if (score[i][j] == score[i-1][j-1] + FIT_SCORE(seqA[i-1], seqB[j-1])) {
+        if (alignment->score[i][j] == alignment->score[i-1][j-1] + FIT_SCORE(seqA[i-1], seqB[j-1])) {
             alignedA[k] = seqA[i-1];
-            alignedHelper[k] = FIT_SCORE(seqA[i-1], seqB[j-1]) == MATCH ? '|' : ' ';
             alignedB[k] = seqB[j-1];
             i--;
             j--;
         }
-        else if (score[i][j] == score[i-1][j] + GAP) {
+        else if (alignment->score[i][j] == alignment->score[i-1][j] + GAP) {
             alignedA[k] = seqA[i-1];
-            alignedHelper[k] = ' ';
             alignedB[k] = '-';
             i--;
         }
         else {
             alignedA[k] = '-';
-            alignedHelper[k] = ' ';
             alignedB[k] = seqB[j-1];
             j--;
 
@@ -114,36 +111,111 @@ void needleman_wunsch_align(char *seqA, char *seqB, int lenA, int lenB) {
     while (i > 0 || j > 0) {
         if (i == 0) {
             alignedA[k] = '-';
-            alignedHelper[k] = ' ';
             alignedB[k] = seqB[j-1];
             j--;
         }
         else if (j == 0) {
             alignedA[k] = seqA[i-1];
-            alignedHelper[k] = ' ';
             alignedB[k] = '-';
             i--;
         }
         k--;
     }
-    
-    // afisare secvente aliniate
-    printf("\nInitial:\n%s\n%s\n\n", seqA, seqB);
-    printf("Aliniere:\n%s\n%s\n%s\n\n", alignedA+k+1, alignedHelper+k+1, alignedB+k+1);
 
-    // dealocare memorie
-    for (i = 0; i <= lenA; i++) {
-        free(score[i]);
-    }
-    free(score);
+    alignment->alignedA = alignedA+k+1;
+    alignment->alignedB = alignedB+k+1;
 
-    free(alignedA);
-    free(alignedHelper);
-    free(alignedB);
+    return alignment;
 }
 
-int main() {
-    // TODO Read from file
-    needleman_wunsch_align("CAGCTAGCG", "CCATACGA", 9, 8);
-    return 0;
+
+void read_sequence(FILE *in, char **descriptor, char **seq, int *len) {
+    char *line = NULL;
+    size_t size = 0;
+    ssize_t read; 
+
+    if ((read = getline(&line, &size, in)) != -1) {
+        line[read-1] = '\0';
+
+        *descriptor = (char *) calloc((size + 1), sizeof(char));
+        strcpy(*descriptor, line);
+        
+        *len = 0;
+        while ((read = getline(&line, &size, in)) != -1 && memchr(line, '>', sizeof(char)) == NULL) {
+            line[read-1] = '\0';
+            
+            if (*len == 0) {
+                *seq = (char *) calloc(size, sizeof(char));
+                strcpy(*seq, line);
+                *len = read-1;
+            }
+            else {
+                *seq = (char *) realloc(*seq, (*len + read) * sizeof(char));
+                strcat(*seq, line);
+                *len += read-1;
+            }
+        }
+
+        // unread last line if it is for another sequence
+        if (memchr(line, '>', sizeof(char)) != NULL) {
+            fseek(in, -1 * read, SEEK_CUR);
+        }
+
+    } else {
+        fprintf(stderr, "Failed to read sequence\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    char *seqA_descriptor, *seqA, *seqB_descriptor, *seqB;
+    struct Alignment *alignment;
+    FILE *input, *output;
+    int i, j, lenA, lenB;
+
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <in.seq>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    input = fopen(argv[1], "r");
+    if (input == NULL) {
+        fprintf(stderr, "Failed to open input file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    read_sequence(input, &seqA_descriptor, &seqA, &lenA);
+    read_sequence(input, &seqB_descriptor, &seqB, &lenB);
+    alignment = needleman_wunsch_align(seqA, seqB, lenA, lenB);
+
+    // print score matrix
+    for (i = 0; i <= lenA; i++) {
+        for (j = 0; j <= lenB; j++) {
+            printf("%3d ", alignment->score[i][j]);
+        }
+        printf("\n");
+    }
+
+    // print sequences alignment
+    printf("\nInitial:\n%s\n%s\n\n", seqA, seqB);
+    printf("Aliniere:\n%s\n%s\n\n", alignment->alignedA, alignment->alignedB);
+
+    // deallocate memory
+    for (i = 0; i <= lenA; i++) {
+        free(alignment->score[i]);
+    }
+    free(alignment->score);
+    // free(alignment->alignedA);
+    // free(alignment->alignedB);
+    free(alignment);
+    free(seqA_descriptor);
+    free(seqB_descriptor);
+    free(seqA);
+    free(seqB);
+
+
+    fclose(input);
+    // fclose(output);
+
+    exit(EXIT_SUCCESS);
 }
